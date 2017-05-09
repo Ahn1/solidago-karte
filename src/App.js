1f2af5c967709da844693063736383800763d8ae
Alexander Bogoslawski <bogoslawski@landaumedia.de>
2017-05-09 10:58:12 +0200

    map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { LatLngBounds } from 'leaflet'
+
 import './App.css';
+import 'leaflet/dist/leaflet.css'
+
+let position = [52.510546, 13.385364];
+
+const community = (lat, long, name) => {
+  return {name, position: [lat, long]}
+}
+
+let communities = [
+  community(52.510546, 13.385364, "Solidago weissensee"),
+  community(52.514546, 13.389364, "Berolina")
+];
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{height: "100%", width: "100%"}}>
+        <Map center={position} zoom={13} maxBounds={[[54.986161, 5.706812], [47.403148, 14.791613]]}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          {communities.map(c =>
+
+            <Marker position={c.position}>
+              <Popup>
+                <span>{c.name}</span>
+              </Popup>
+            </Marker>
+
+
+          ) }
+
+        </Map>
+
+
       </div>
     );
   }
